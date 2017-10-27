b38bc07f076a10a52b97f33b58de3c505f47783b
Alan <alanvallve@gmail.com>
2017-10-27 13:09:25 +0200

    casi acabado

--- app/src/main/java/com/alanv/practicaandroid/TikTakToeActivity.java
+++ app/src/main/java/com/alanv/practicaandroid/TikTakToeActivity.java
@@ -2,17 +2,51 @@
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
 
          public class TikTakToeActivity extends AppCompatActivity {
 
-    Button button;
+    List<Button> buttons = new ArrayList();
+
+    boolean who = false;
+    int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tik_tak_toe);
+    }
 
-        button = (Button) findViewById(R.id.zero);
+    public void xOrO(View view){
+
+        Button c = (Button) view;
+
+        if (c.getText().equals("")){
+            if(!who){
+                c.setText("X");
+                who = true;
+            }else {
+                c.setText("O");
+                who = false;
+            }
+            count++;
+            buttons.add(c);
+
+            if(count == 9){
+                restart();
+            }
+        }
+    }
+
+    private void restart(){
+        for(Button b : buttons){
+            b.setText("");
+        }
+        buttons.removeAll(buttons);
+        count = 0;
     }
 }
